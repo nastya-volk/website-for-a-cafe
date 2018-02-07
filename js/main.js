2c421a802d6db75dbe8459a1547acc9a52b6d986
NastyaVolk <nastysha19_97@mail.ru>
2018-02-07 12:01:51 +0300

    Website for a coffeeshop

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.onload = function animatePage() {
+document.getElementById('name').classList.add('slideInUp');
+document.getElementById('down-arrow').classList.add('bounce');
+document.getElementById('down-arrow').classList.add('infinite');
+}
